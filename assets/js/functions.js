d3e0021246e894edb4a8b5b6740e3075be520e6d
IhorZhuk <ihor.zhuk92@gmail.com>
2016-01-22 07:55:17 +0200

    fixed font

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,10 +2,32 @@
   'use strict';
 
   $(document).ready(function () {
-
-
-
-
+    
+    function blueasyTabs() {
+      var $wrapper = $('.tab-wrapper'),
+          $menu = $wrapper.find('.tab-menu li'),
+          $content = $wrapper.find('.thumbs-row');
+      
+      $content.not(':first-of-type').hide();
+      $content.each(function(i) {
+        $(this).attr('data-tab', 'tab-'+i);
+      });
+      $menu.each(function(i) {
+        $(this).attr('data-tab', 'tab-'+i);
+      });
+      
+      $menu.click(function() {
+        var $getWrapper = $(this).closest($wrapper),
+            dataTab = $(this).attr('data-tab');
+        
+        $getWrapper.find($menu).find('a').removeClass('active');
+        $(this).find('a').addClass('active');
+        
+        $getWrapper.find($content).hide();
+        $getWrapper.find($content).filter('[data-tab='+dataTab+']').show();
+      });
+    }
+    blueasyTabs();
 
   }); //end ready
 
