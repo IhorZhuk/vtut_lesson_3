bbfc7eb84ddacf070459f53097832e102c1be588
IhorZhuk <ihor.zhuk92@gmail.com>
2016-02-19 15:48:01 +0100

    end of 9 part

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -28,6 +28,23 @@
       });
     }
     blueasyTabs();
+    //end blueasyTabs
+    
+    function blueasyMenu() {
+      var $trigger = $('.trigger-nav'),
+          $menu = $('.trigger-victim');
+      
+      $trigger.click(function() {
+        $(this).next($menu).slideToggle();
+      });
+      
+      $(window).resize(function() {
+        if ( $(window).width() > 992 ) {
+          $menu.removeAttr('style');
+        }
+      });
+    }
+    blueasyMenu();
 
   }); //end ready
 
